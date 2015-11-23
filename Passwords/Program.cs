c170338a13e74f6106481b88c650f29fb57d86fb
Wolfie13 <wwwolfie13@gmail.com>
2015-11-23 12:38:29 +0000

    Passwords v1

--- Passwords/Program.cs
+++ Passwords/Program.cs
@@ -8,8 +8,113 @@ namespace Passwords
 {
     class Program
     {
+        private static String[] words =
+        {
+            "about",
+            "after",
+            "again",
+            "below",
+            "could",
+            "every",
+            "first",
+            "found",
+            "great",
+            "house",
+            "large",
+            "learn",
+            "never",
+            "other",
+            "place",
+            "plant",
+            "point",
+            "right",
+            "small",
+            "sound",
+            "spell",
+            "still",
+            "study",
+            "their",
+            "there",
+            "these",
+            "thing",
+            "think",
+            "three",
+            "water",
+            "where",
+            "which",
+            "world",
+            "would",
+            "write"
+        };
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Program program = new Program();
+                program.run();
+            }
+        }
+
+        public void run()
+        {
+            Console.WriteLine("Tumbler 1");
+            tumblers[0] = Console.ReadLine();
+            printAll(candidateWords());
+            Console.WriteLine("Tumbler 2");
+            tumblers[1] = Console.ReadLine();
+            printAll(candidateWords());
+            Console.WriteLine("Tumbler 3");
+            tumblers[2] = Console.ReadLine();
+            printAll(candidateWords());
+            Console.WriteLine("Tumbler 4");
+            tumblers[3] = Console.ReadLine();
+            printAll(candidateWords());
+            Console.WriteLine("Tumbler 5");
+            tumblers[4] = Console.ReadLine();
+            printAll(candidateWords());
+        }
+
+        private String[] tumblers = new String[5];
+
+        private String validateTumbler(int n, String s)
+        {
+            if (tumblers[n] != null && s != null)
+            {
+                if (!tumblers[n].Contains(s[n]))
+                {
+                    return null;
+                }
+            }
+            return s;
+        }
+
+        private List<String> candidateWords()
+        {
+            List<String> result = new List<String>();
+            foreach (String possibleWord in words)
+            {
+                String candidate = validateTumbler(0, possibleWord);
+                candidate = validateTumbler(1, candidate);
+                candidate = validateTumbler(2, candidate);
+                candidate = validateTumbler(3, candidate);
+                candidate = validateTumbler(4, candidate);
+                if (candidate != null)
+                {
+                    result.Add(candidate);
+                }
+            }
+            return result;
+        }
+
+        private void printAll(List<String> toPrint)
+        {
+            foreach (String w in toPrint)
+            {
+                Console.Write(w);
+                Console.Write(", ");
+            }
+            Console.WriteLine();
         }
     }
 }
